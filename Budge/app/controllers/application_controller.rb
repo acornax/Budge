26adf9dfdb94e17bb8902f6df473a49158b07f4a
acornax <acornax@gmail.com>
2013-07-08 19:21:49 -0700

    Update application_controller.rb

--- Budge/app/controllers/application_controller.rb
+++ Budge/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
 	def current_user
 		if cookies[:auth_token]
-			current_user = User.find_by_auth_token(cookies[:auth_token])
+			@current_user = User.find_by_auth_token(cookies[:auth_token])
 		end
 	end
 end
