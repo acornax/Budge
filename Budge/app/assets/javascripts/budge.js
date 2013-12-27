ba2db91d84f1ada441ed150c01ca137a327cbff1
Andrew Cornax <acornax@gmail.com>
2013-12-26 22:48:57 -0800

    Added jqplot, and a factory for data

--- Budge/app/assets/javascripts/budge.js
+++ Budge/app/assets/javascripts/budge.js
@@ -3,12 +3,14 @@
 
   app = angular.module("Budge", ["ngResource"]);
 
-  this.ExpensesCtrl = function($scope, $resource) {
-    var Expense;
+  app.factory('Expenses', function() {
+       return [{amount: 1, date: new Date("5/13/2013")}, {amount: 10, date: new Date("11/13/2013")}, {amount: 2, date: new Date("12/03/2013")}]
+  });
 
-    Expense = $resource("/expenses/:id", {id: "@id"}, {update: { method: "PUT"}});
+  app.controller("ExpensesCtrl" , function($scope, Expenses) {
 
-    $scope.expenses = Expense.query();
+    $scope.expenses = Expenses;
+
     return $scope.addExpense = function() {
       var expense;
 
@@ -18,13 +20,44 @@
       $scope.expenses.push($scope.newExpense);
       return $scope.newExpense = {};
     };
-  };
+  });
 
-  this.SummaryCtrl = function($scope, $resource) {
+  app.controller("SummaryCtrl", function($scope, Expenses) {
 
-    var Expense = $resource('expenses/:id', {id: "@id"}, {update : {method : "PUT"}});
-    $scope.expenses = Expense.query();
-  };
+    $scope.expenses = Expenses;
+
+    $scope.getTotals = function () {
+     var totals = [0,0,0,0,0,0,0,0,0,0,0,0];
+     for (i = 0; i < $scope.expenses.length; i++){
+        month = $scope.expenses[i].date.getMonth();
+        totals[month] += $scope.expenses[i].amount;
+     }
+     return [totals];
+    }
+  });
+
+  app.directive('plot', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          var ticks = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+          $.jqplot($(element).attr("id"),  scope.getTotals(), {
+            series:[{renderer:$.jqplot.BarRenderer
+
+            }],
+
+            axes: {
+              xaxis:{
+                renderer: $.jqplot.CategoryAxisRenderer,
+                ticks: ticks
+              }
+            } 
+
+          });
+        }
+    };
+  }); 
 
 }).call(this);
 
